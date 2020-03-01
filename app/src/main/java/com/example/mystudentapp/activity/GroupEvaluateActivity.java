8eba6869b3d015cb2ab8fe023b70b92fb2ecf108
sjw <1748218486@qq.com>
2020-03-01 17:07:44 +0800

    小组，老师

--- app/src/main/java/com/example/mystudentapp/activity/GroupEvaluateActivity.java
+++ app/src/main/java/com/example/mystudentapp/activity/GroupEvaluateActivity.java
@@ -1,27 +1,47 @@
 package com.example.mystudentapp.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.example.mystudentapp.R;
+import com.example.mystudentapp.adapter.StudentHuPinAdater;
 import com.example.mystudentapp.base.BaseActivity;
+import com.example.mystudentapp.base.BaseMeassage;
+import com.example.mystudentapp.bean.User;
+import com.example.mystudentapp.db.bean.Student;
+import com.example.mystudentapp.db.ctrl.XiaoZuCtrl;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupEvaluateActivity extends BaseActivity implements View.OnClickListener {
     //小组互评界面
 
     private ImageView ivBack;
+    private User user;
+    private ListView lvStudent;  //除自己以为的学生列表
+    private List<Student> list;
+    private StudentHuPinAdater adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_evaluate);
+        user= BaseMeassage.INSTANCE.getUser();
         initView();
+        initData();
         initListener();
     }
+    private void initData() {
 
+        list=new ArrayList<>();
+        list= XiaoZuCtrl.selectOther(user.getBianHao());
+        adapter.setList(list);
+
+    }
     private void initListener() {
         ivBack.setOnClickListener(this);
 
@@ -29,6 +49,9 @@ public class GroupEvaluateActivity extends BaseActivity implements View.OnClickL
 
     private void initView() {
         ivBack=findViewById(R.id.iv_back);
+        lvStudent=findViewById(R.id.lv_student);
+        adapter=new StudentHuPinAdater(this);
+        lvStudent.setAdapter(adapter);
 
 
     }
--- app/src/main/java/com/example/mystudentapp/activity/TeacherEvaluateActivity.java
+++ app/src/main/java/com/example/mystudentapp/activity/TeacherEvaluateActivity.java
@@ -5,25 +5,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.example.mystudentapp.R;
+import com.example.mystudentapp.adapter.StudentHuPinAdater;
 import com.example.mystudentapp.base.BaseActivity;
+import com.example.mystudentapp.base.BaseMeassage;
+import com.example.mystudentapp.bean.User;
+import com.example.mystudentapp.db.bean.Student;
+import com.example.mystudentapp.db.ctrl.TeacherPingJiaCtrl;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TeacherEvaluateActivity extends BaseActivity implements View.OnClickListener {
     //教师评价界面
 
     private ImageView ivBack;
+    private User user;
+    private ListView lvStudent;  //除自己以为的学生列表
+    private List<Student> list;
+    private StudentHuPinAdater adapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_evaluate);
-
+        user= BaseMeassage.INSTANCE.getUser();
         initView();
+        initData();
         initListener();
     }
+    private void initData() {
 
+        list=new ArrayList<>();
+        list= TeacherPingJiaCtrl.selectOther(user.getBianHao());
+        adapter.setList(list);
+
+    }
     private void initListener() {
         ivBack.setOnClickListener(this);
 
@@ -31,8 +51,9 @@ public class TeacherEvaluateActivity extends BaseActivity implements View.OnClic
 
     private void initView() {
         ivBack=findViewById(R.id.iv_back);
-
-
+        lvStudent=findViewById(R.id.lv_student);
+        adapter=new StudentHuPinAdater(this);
+        lvStudent.setAdapter(adapter);
     }
 
     @Override
